b20ebf200f70dee0aa34beed4598fb684b710fae
2077collective <dimmstreet@gmail.com>
2024-08-10 08:13:40 +0100

    chore(fix): UI update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      p: {
+        custom: 'clamp(1rem, 0.6522rem + 1.7391vw, 2rem)',
+      },
+    },
   },
   plugins: [],
   darkMode: "class",
